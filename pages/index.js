6a17c6603b35f6c5936a66244ce55c7feadec63a
Robert Hull <robertjhull@protonmail.com>
2021-02-02 18:02:06 -0800

    first validation attempt

--- pages/index.js
+++ pages/index.js
@@ -2,23 +2,36 @@ import { useState } from 'react';
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 
-const nums = Array(1).fill().map(() => ~~(Math.random() * 100));
+let nums = Array(1).fill().map(() => ~~(Math.random() * 100));
 
 const Home = () => {
 
-  const correct = <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="green" class="bi bi-check" viewBox="0 0 16 16"><path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z"/></svg>
+  const correct = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="green" class="bi bi-check" viewBox="0 0 16 16"><path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z"/></svg>`
 
-  const wrong = <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="red" class="bi bi-x" viewBox="0 0 16 16"><path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/></svg>
+  const wrong = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="red" class="bi bi-x" viewBox="0 0 16 16"><path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/></svg>`
 
-  let [binAnswer, setBinAnswer] = useState(0);
+  let [binAnswer, setBinAnswer] = useState();
+  let [hexAnswer, setHexAnswer] = useState();
+  let [octAnswer, setOctAnswer] = useState();
 
   const checkAnswers = (e) => {
     e.preventDefault();
     if (binAnswer == nums[0].toString(2)) {
-      console.log("Correct!");
+      document.getElementById("bin-ans").innerHTML = correct;
+    } else {
+      document.getElementById("bin-ans").innerHTML = wrong;
     }
-    else {
-      console.log("inccorect");
+
+    if (hexAnswer == nums[0].toString(16)) {
+      document.getElementById("hex-ans").innerHTML = correct;
+    } else {
+      document.getElementById("hex-ans").innerHTML = wrong;
+    }
+
+    if (octAnswer == nums[0].toString(8)) {
+      document.getElementById("oct-ans").innerHTML = correct;
+    } else {
+      document.getElementById("oct-ans").innerHTML = wrong;
     }
   }
 
@@ -51,11 +64,11 @@ const Home = () => {
           <tr key={i}>
             <td>{num}</td>
             <td><input type="text" name={"bin" + i} onChange={ e => setBinAnswer(e.target.value) }></input></td>
-            <td>{num.toString(2)}</td>
-            <td><input type="text" name={"hex" + i}></input></td>
-            <td>{num.toString(16)}</td>
-            <td><input type="text" name={"oct" + i}></input></td>
-            <td>{num.toString(8)}</td>
+            <td className={styles.answer} id="bin-ans"></td>
+            <td><input type="text" name={"hex" + i} onChange={ e => setHexAnswer(e.target.value) }></input></td>
+            <td className={styles.answer} id="hex-ans"></td>
+            <td><input type="text" name={"oct" + i} onChange={ e => setOctAnswer(e.target.value) }></input></td>
+            <td className={styles.answer} id="oct-ans"></td>
           </tr>)
         }
         </tbody>
