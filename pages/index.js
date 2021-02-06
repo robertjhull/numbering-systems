64a44b015b35c6c7479b35cd74f1fadfd8d03b73
Robert Hull <robertjhull@protonmail.com>
2021-02-05 17:49:43 -0800

    rough quiz select form

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,31 @@ const Home = () => {
         <title>Base Conversion Practice</title>
       </Head>
       <main className={styles.main}>
-        
+        <div className={styles.card}>
+          <form>
+            <label for="question-amount">Number of Questions</label>
+            <input type="number" name="question-amount"></input>
+            <h4>Convert From:</h4>
+            <input type="checkbox" name="binary-check"></input>
+            <label for="binary-check">Binary</label>
+            <input type="checkbox" name="decimal-check"></input>
+            <label for="binary-check">Decimal</label>
+            <input type="checkbox" name="octal-check"></input>
+            <label for="binary-check">Octal</label>
+            <input type="checkbox" name="hex-check"></input>
+            <label for="binary-check">Hex</label>
+            <h4>Convert To:</h4>
+            <input type="checkbox" name="binary-check"></input>
+            <label for="binary-check">Binary</label>
+            <input type="checkbox" name="decimal-check"></input>
+            <label for="binary-check">Decimal</label>
+            <input type="checkbox" name="octal-check"></input>
+            <label for="binary-check">Octal</label>
+            <input type="checkbox" name="hex-check"></input>
+            <label for="binary-check">Hex</label>
+            <input type="Submit"></input>
+          </form>
+        </div>
       </main>
     </div>
   )
