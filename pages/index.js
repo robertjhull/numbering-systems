42b98327555fa307242c1ab0f7760c1446ed7b8b
Robert Hull <robertjhull@protonmail.com>
2021-02-08 23:45:54 -0800

    updated css/layout

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,14 @@ import styles from '../styles/Home.module.css';
 
 const Home = () => {
 
+  let [questionAmount, setQuestionAmount] = useState(10);
+  let [conversionFrom, setConversionFrom] = useState([]);
+  let [conversionTo, setConversionTo] = useState([]);
+
+  const onSubmitFormHandler = function(e) {
+    console.log(questionAmount, conversionFrom, conversionTo)
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -11,28 +19,54 @@ const Home = () => {
       </Head>
       <main className={styles.main}>
         <div className={styles.card}>
-          <form>
-            <label for="question-amount">Number of Questions</label>
-            <input type="number" name="question-amount"></input>
-            <h4>Convert From:</h4>
-            <input type="checkbox" name="binary-check"></input>
-            <label for="binary-check">Binary</label>
-            <input type="checkbox" name="decimal-check"></input>
-            <label for="binary-check">Decimal</label>
-            <input type="checkbox" name="octal-check"></input>
-            <label for="binary-check">Octal</label>
-            <input type="checkbox" name="hex-check"></input>
-            <label for="binary-check">Hex</label>
-            <h4>Convert To:</h4>
-            <input type="checkbox" name="binary-check"></input>
-            <label for="binary-check">Binary</label>
-            <input type="checkbox" name="decimal-check"></input>
-            <label for="binary-check">Decimal</label>
-            <input type="checkbox" name="octal-check"></input>
-            <label for="binary-check">Octal</label>
-            <input type="checkbox" name="hex-check"></input>
-            <label for="binary-check">Hex</label>
-            <input type="Submit"></input>
+          <form onSubmit={ onSubmitFormHandler } action="/quiz">
+            <div className={styles.formgroup}>
+              <label htmlFor="question-amount">Number of Questions</label><br />
+              <input type="number" id={styles.questioninput} name="question-amount" defaultValue="10" onChange={e => setQuestionAmount(e.target.value)}></input>
+            </div>
+            <div className={styles.cardcol}>
+              <div className={styles.formgroup}>
+                <p className={styles.label}>Convert From:</p>
+                <div>
+                  <input checked type="checkbox" name="binary-check"></input>
+                  <label htmlFor="binary-check">Binary</label>
+                </div>
+                <div>
+                  <input checked type="checkbox" name="decimal-check"></input>
+                  <label htmlFor="binary-check">Decimal</label>
+                </div>
+                <div>
+                  <input checked type="checkbox" name="octal-check"></input>
+                  <label htmlFor="binary-check">Octal</label>
+                </div>
+                <div>
+                  <input checked type="checkbox" name="hex-check"></input>
+                  <label htmlFor="binary-check">Hex</label>
+                </div>
+              </div>
+              <div className={styles.formgroup}>
+                <p className={styles.label}>Convert To:</p>
+                <div>
+                  <input checked type="checkbox" name="binary-check"></input>
+                  <label htmlFor="binary-check">Binary</label>
+                </div>
+                <div>
+                  <input checked type="checkbox" name="decimal-check"></input>
+                  <label htmlFor="binary-check">Decimal</label>
+                </div>
+                <div>
+                  <input checked type="checkbox" name="octal-check"></input>
+                  <label htmlFor="binary-check">Octal</label>
+                </div>
+                <div>
+                  <input checked type="checkbox" name="hex-check"></input>
+                  <label htmlFor="binary-check">Hex</label>
+                </div>
+              </div>
+            </div>
+            <div className={styles.formgroup}>
+              <input className={styles.submit} type="submit" value="Generate Quiz"></input>
+            </div>
           </form>
         </div>
       </main>
