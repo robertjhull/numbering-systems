a13734e10bf8164897bc8b74e2efb3c12ab1fed6
robertjhull <67487694+robertjhull@users.noreply.github.com>
2021-05-20 23:34:39 -0700

    Remove unused code

--- utils/toRomanNumeral.js
+++ utils/toRomanNumeral.js
@@ -1,36 +1,3 @@
-// Number.prototype.toBase = function(
-//     base = 10,
-//     prefix = false,
-// ) {
-//     let num = this;
-//     let result = [];
-
-//     while (num > 0) {
-//         let remainder = num % base;
-//         if (base == 16 && remainder > 9) { 
-//             remainder = ["A", "B", "C", "D", "E", "F"][remainder - 10];
-//         }
-//         num = Math.floor(num / base)
-//         result.unshift(remainder)
-//     }
-
-//     if (prefix) {
-//         switch(base) {
-//             case 2: 
-//                 result.unshift("0b");
-//                 break;
-//             case 8:
-//                 result.unshift("Oo");
-//                 break;
-//             case 16:
-//                 result.unshift("Ox");
-//                 break;
-//         }
-//     }
-
-//     return result.join("");
-// }
-
 Number.prototype.toRomanNumeral = function() {
     let num = this;
     let result = [];
@@ -49,20 +16,3 @@ Number.prototype.toRomanNumeral = function() {
     }
     return result.join("");
 }
-
-
-// console.log((156).toBase(2)) // Should be 10011100
-// console.log((343).toBase(2)) // 101010111
-// console.log((7652).toBase(2)) // 1110111100100
-
-// console.log((156).toBase(8)) // Should be 234
-// console.log((343).toBase(8)) // 527
-// console.log((7652).toBase(8)) // 16744
-
-// console.log((156).toBase(16)) // Should be 9C
-// console.log((343).toBase(16)) // 157
-// console.log((7652).toBase(16)) // 1DE4
-
-// console.log((3549).toRomanNumeral()) // should be MMMDXLIX
-// console.log((50).toRomanNumeral())
-// console.log((234).toRomanNumeral())
